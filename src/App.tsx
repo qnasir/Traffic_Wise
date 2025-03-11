9b020d2ea8ab47ca79284a2ebb3f0d58b70f3995
qnasir57575 <qnasir57575@gmail.com>
2025-03-11 09:42:21 +0530

    User Profile Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AlertsProvider } from "./context/AlertsContext";
+import { AuthProvider } from "./context/AuthContext";
 import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <AlertsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-    </AlertsProvider>
+    <AuthProvider>
+      <AlertsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+          </Routes>
+        </BrowserRouter>
+      </AlertsProvider>
+    </AuthProvider>
   </QueryClientProvider>
 );
 
